0daed522c7708ec73e6475b71441636205c50b38
Justin Thomas <jthomas0209@gmail.com>
2016-02-02 12:06:48 -0500

    Done

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -38,8 +38,8 @@ describe 'form' do
 
     visit edit_post_path(@post)
 
-    fill_in 'post[title]', with: "My edit"
-    fill_in 'post[description]', with: "My post description"
+    fill_in 'title', with: "My edit"
+    fill_in 'description', with: "My post description"
 
     click_on "Update Post"
 
