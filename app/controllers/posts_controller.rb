268053f9792179b1eba03858cbf247fb0d00bc85
Deborah Seow <deborahseow9@gmail.com>
2018-06-15 20:58:25 -0500

    Added Model Class Methods Lesson code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,21 @@
 class PostsController < ApplicationController
+
   def index
-    @posts = Post.all
+    @authors = Author.all
+
+    # filter the @posts list based on user input
+    if !params[:author].blank?
+      @posts = Post.by_author(params[:author])
+    elsif !params[:date].blank?
+      if params[:date] == "Today"
+        @posts = Post.from_today
+      else
+        @posts = Post.old_news
+      end
+    else
+      # if no filters are applied, show all posts
+      @posts = Post.all
+    end
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -13,4 +13,15 @@
 class Post < ActiveRecord::Base
   belongs_to :author
 
+  def self.by_author(author_id)
+    where(author: author_id)
+  end
+
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
+  end
+
+  def self.old_news
+    where("created_at <?", Time.zone.today.beginning_of_day)
+  end
 end
