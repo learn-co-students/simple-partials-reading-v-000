5fb63304647e8a189f6982ac41ea65970ae32b5e
sdcro <sdcrouse91@gmail.com>
2019-12-22 01:37:37 -0800

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,19 +7,40 @@ describe 'navigate' do
     @post = Post.create(title: "A Time To Kill", description: "A Time to Kill is a 1989 legal suspense thriller by John Grisham. It was Grisham's first novel. The novel was rejected by many publishers before Wynwood Press (located in New York) eventually gave it a modest 5,000-copy printing. After The Firm, The Pelican Brief, and The Client became bestsellers, interest in A Time to Kill grew; the book was republished by Doubleday in hardcover and, later, by Dell Publishing in paperback, and itself became a bestseller. This made Grisham extremely popular among readers.", author_id: 1)
   end
 
-  it 'shows the title on the show page in a h1 tag' do
-    visit "/posts/#{@post.id}"
-    expect(page).to have_css("h1", text: "A Time To Kill")
-  end
+  context 'to post show page' do
+    it 'returns a 200 status code' do
+      visit "/posts/#{@post.id}"
+      expect(page.status_code).to eq(200)
+    end
+  
+    it 'shows the title on the show page in a h1 tag' do
+      visit "/posts/#{@post.id}"
+      expect(page).to have_css("h1", text: "A Time To Kill")
+    end  
+  
+    it 'shows the description on the show page in a p tag' do
+      visit "/posts/#{@post.id}"
+      expect(page).to have_css("p", text: "A Time to Kill is a 1989 legal suspense thriller by John Grisham. It was Grisham's first novel. The novel was rejected by many publishers before Wynwood Press (located in New York) eventually gave it a modest 5,000-copy printing. After The Firm, The Pelican Brief, and The Client became bestsellers, interest in A Time to Kill grew; the book was republished by Doubleday in hardcover and, later, by Dell Publishing in paperback, and itself became a bestseller. This made Grisham extremely popular among readers.")
+    end
 
-  it 'to post pages' do
-    visit "/posts/#{@post.id}"
-    expect(page.status_code).to eq(200)
+    it "shows the name and hometown of the post's author" do
+      visit "/posts/#{@post.id}"
+      expect(page).to have_content(@post.author.name)
+      expect(page).to have_content(@post.author.hometown)
+    end
   end
+  
+  context 'to author show page' do
+    it 'returns a 200 status code' do
+      visit "/authors/#{@author.id}"
+      expect(page.status_code).to eq(200)
+    end
 
-  it 'shows the description on the show page in a p tag' do
-    visit "/posts/#{@post.id}"
-    expect(page).to have_css("p", text: "A Time to Kill is a 1989 legal suspense thriller by John Grisham. It was Grisham's first novel. The novel was rejected by many publishers before Wynwood Press (located in New York) eventually gave it a modest 5,000-copy printing. After The Firm, The Pelican Brief, and The Client became bestsellers, interest in A Time to Kill grew; the book was republished by Doubleday in hardcover and, later, by Dell Publishing in paperback, and itself became a bestseller. This made Grisham extremely popular among readers.")
+    it "shows the author's name and hometown" do
+      visit "/authors/#{@author.id}"
+      expect(page).to have_content(@author.name)
+      expect(page).to have_content(@author.hometown)
+    end
   end
 end
 
