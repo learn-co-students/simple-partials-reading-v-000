bdbf53aaed849f8bdd070d2e9b1d3cada419cc7e
sdcro <sdcrouse91@gmail.com>
2019-12-22 00:56:41 -0800

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -40,7 +40,7 @@ describe 'form' do
     expect(page).to have_content("My post title")
   end
 
-  it 'shows a new form that submits content and redirects and prints out params' do
+  it 'shows an edit form that submits content and redirects and prints out params' do
 
     visit edit_post_path(@post)
 
