db5814f3613fc211c9008ec1f9e70ee3523d05d5
gigi <mmgamao1@gmail.com>
2020-01-15 13:50:09 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,10 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
-
+  
   def show
     @post = Post.find(params[:id])
+    @author = @post.author
   end
 
   def new
