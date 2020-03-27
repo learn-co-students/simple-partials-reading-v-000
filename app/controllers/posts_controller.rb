f06606bbdbb4dea788bde4dcd895c9816f62f8f0
Sara Harvey <saraharveynyc@gmail.com>
2020-03-27 17:15:11 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,9 +3,10 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def show
-    @post = Post.find(params[:id])
-  end
+def show
+  @post = Post.find(params[:id])
+  @author = @post.author
+end
 
   def new
     @post = Post.new
